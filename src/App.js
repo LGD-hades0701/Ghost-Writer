13593eab658d018b86ffb4bf2d7d1601efb96a2b
dashdev0129 <dashdev129129@gmail.com>
2023-02-10 18:47:27 -0500

    Initial Redux Structure added!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components"
+
+const StyledButton = styled.button`
+  background-color: black;
+  font-size: 32px;
+  color: white;
+  background-color: ${props => props.bg};
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      </header>
+    <div>
+      <StyledButton bg = "black">Login</StyledButton>
     </div>
   );
 }
