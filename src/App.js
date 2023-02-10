54f848677dd5ed98922f4edabf1953e54b96d357
dashdev0129 <dashdev129129@gmail.com>
2023-02-10 15:35:22 -0500

    App.js is edited.

--- src/App.js
+++ src/App.js
@@ -14,9 +14,9 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        > */}
+        >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
