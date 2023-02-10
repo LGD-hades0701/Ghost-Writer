69ae641eabc5b400e365602c052ef75ded4daccd
dashdev0129 <dashdev129129@gmail.com>
2023-02-10 15:04:26 -0500

    front_end branch test

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+        > */}
           Learn React
         </a>
       </header>
